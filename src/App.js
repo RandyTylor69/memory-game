272bde0ec0d5f55c0f643f95f333060c0f5c2103
Depayss <Christophermaomao@gmail.com>
2025-06-10 20:34:36 -0400

    sass setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './App.scss'
+import Form from "./components/Form"
+import React from 'react';
 
 function App() {
+
+  // pre-game settings
+  const [isGameOn, setIsGameOn] = React.useState(false)
+  function startGame(e){
+    e.preventDefault();
+    setIsGameOn(true)
+  }
+
+  // interacting with cards
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>The Memory Game of All Time</h1>
+      <Form />
+    </main>
   );
 }
 
